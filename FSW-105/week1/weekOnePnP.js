1931d55fd9b4bcd5948f3e2a729478ca60be138b
Samuelscheffer <samuel.scheffer@bryanuniversity.edu>
2021-01-23 22:43:13 -0600

    commit
    commit

---
+++ FSW-105/week1/weekOnePnP.js
@@ -0,0 +1,14 @@
+var shopper = {
+    firstName: 'James' ,
+    lastName: 'Smith',
+    shopperID: 01, 
+    age: 29 > 18,
+    groceryCart: ['Bread' , 'Milk' , 'Eggs'] ,
+    fullName : function(){
+        return this.firstName + "" + this.lastName;
+    }
+
+}
+ console.log(shopper)
+
+
